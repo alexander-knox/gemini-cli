529b2d90e4cc09de0e610b3d6dedbcb2f80f9d83
Alexander Knox <149740967+alexander-knox@users.noreply.github.com>
2024-02-24 22:43:36 -0600

    Refactor gemini.py
    refactor the main script into distinct functions for readability

--- gemini.py
+++ gemini.py
@@ -1,4 +1,5 @@
-# Import necessary libraries
+# Libraries
+
 import os
 import pathlib
 import textwrap
@@ -8,20 +9,13 @@ from dotenv import load_dotenv
 from IPython.display import display
 from IPython.display import Markdown
 
+# -----------------------------------------------------------------------
+
+# Boilerplate
+
 # Get API Key from .env
 load_dotenv()
 key = os.getenv("KEY")
-
-
-# Uncomment to verify key import
-# print(key)
-# print(os.environ)
-
-
-# Markdown function Google told me I needed.
-def to_markdown(text):
-  text = text.replace('•', '  *')
-  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
 # Set API key and select model
 genai.configure(api_key=key)
@@ -31,41 +25,66 @@ model = genai.GenerativeModel('gemini-pro')
 preprompt_file = "preprompt.json"
 feedback_file = "feedback.txt"
 
-# Initiate while loop for continous chat 
-# Input 'exit' to break loop
-while True:
+# Markdown function Google told me I needed.
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
-    # Accept current_prompt
-    current_prompt = input("User: ")
+# -----------------------------------------------------------------------
+
+def get_user_input():
+
+    # Pass the user input through a variety of built in string methods to improve consistency and reduce risk of violating JSON syntax.
+    raw_prompt = input("User: ")
+    cleaned_prompt = raw_prompt.lower().strip().replace(':','').replace('"','').replace('{', '').replace('}', '').replace("'",'')
+    current_prompt = cleaned_prompt
+
+    # Pass the sanitized user input to the io_handler, unless the user is trying to exit the script.
+    if current_prompt == "exit":
+        return
+    else:
+        log_user_input(current_prompt)
+
+# -----------------------------------------------------------------------
+
+def log_user_input(inputs):
         
-    # Load the pre-prompt file in preparation for inserting user input
-    with open(preprompt_file, "r") as f:
-        preprompt = json.load(f)
-    # Check preprompt payload
-    # print(preprompt)
+        # Set inputs to current prompt
+        current_prompt = inputs
 
-    # Set value of current prompt in JSON file as the user input && add to context
-    preprompt["current_prompt"]["current_prompt"] = current_prompt
-    preprompt["context"]["context"].append({"message": f"User: {current_prompt}"})
+        # Load the pre-prompt file in preparation for inserting user input
+        with open(preprompt_file, "r") as f:
+            preprompt = json.load(f)
 
-    with open(preprompt_file, "w") as f:
-        json.dump(preprompt, f, indent=4)
+        # Set value of current prompt in JSON file as the user input && add to context
+        preprompt["current_prompt"]["current_prompt"] = current_prompt
+        preprompt["context"]["context"].append({"message": f"User: {current_prompt}"})
 
-    # Open the pre-prompt file in preparation for feeding to the model
+        with open(preprompt_file, "w") as f:
+            json.dump(preprompt, f, indent=4)
+
+        send_model_input()
+
+# -----------------------------------------------------------------------
+
+def send_model_input():
+
+        # Open the pre-prompt file in preparation for feeding to the model
     with open(preprompt_file, "r") as f:
         preprompt = json.load(f)
     
     # Initialize variable for prompt as raw text
     prompt = json.dumps(preprompt, indent=None)
 
-    # print(prompt)
-
     # Feed prompt to the model && print the response for the user && collect API feedback
     response = model.generate_content(prompt)
     feedback = response.prompt_feedback
-    print(response)
-    # print response
-    print(f"\nGemini: {response.text}\n")
+
+    log_model_output(response,feedback,preprompt)
+
+# -----------------------------------------------------------------------
+
+def log_model_output(response,feedback,preprompt):
 
     # Append model response to context in preprompt file
     preprompt["context"]["context"].append({"message": f"Gemini: {response.text}"})
@@ -74,7 +93,11 @@ while True:
 
     with open(feedback_file, "w") as f:
         f.write(str(feedback))
-    
-    # Loop break
-    if current_prompt.lower() == "exit":
-        break
+
+    print(f"\nGemini: {response.text}\n")
+
+    get_user_input()
+
+# -----------------------------------------------------------------------
+
+get_user_input()
